8eb404b8f2f0a0ca46cf147e6c8ad61af508fdfc
Ivan Paulin <paulin.ivan.1@gmail.com>
2019-06-06 14:41:29 +0200

    adapt code to WP standards

--- includes/admin/WCCBPSettings.php
+++ includes/admin/WCCBPSettings.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Admin settings in WooCommerce
  */
@@ -8,15 +7,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Create WC admin settings page and fields
+ */
 class WCCBPSettings extends WC_Settings_Page {
 
-    protected $id;
+	/**
+	 * Settings page ID
+	 *
+	 * @var $id String
+	 */
+	protected $id;
 
-    public function __construct()
-    {
-        $this->id = 'wccbp';
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->id = 'wccbp';
 
-        $this->label = __( 'WCCBP', 'my-textdomain' );
+		$this->label = __( 'WCCBP', 'my-textdomain' );
 
 		add_filter( 'woocommerce_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
 
@@ -24,11 +33,11 @@ class WCCBPSettings extends WC_Settings_Page {
 
 		add_action( 'woocommerce_sections_' . $this->id, array( $this, 'output_sections' ) );
 
-        add_action('woocommerce_settings_tabs_' . $this->id, array($this, 'addSectionToTab'));
+		add_action( 'woocommerce_settings_tabs_' . $this->id, array( $this, 'add_section_to_tab' ) );
 
-        add_action('woocommerce_update_options_' . $this->id, array($this, 'updateOptions'));
+		add_action( 'woocommerce_update_options_' . $this->id, array( $this, 'update_options' ) );
 	}
-	
+
 	/**
 	 * Output the settings
 	 */
@@ -41,66 +50,64 @@ class WCCBPSettings extends WC_Settings_Page {
 	}
 
 
-    /**
-     * Cerate input field for every available payment gateway
-     *
-     * @return $fields array
-     */
-    public function createFields()
-    {
-        $available_gateways = WC()->payment_gateways->payment_gateways();
+	/**
+	 * Cerate input field for every available payment gateway
+	 *
+	 * @return array
+	 */
+	public function create_fields() {
+		$available_gateways = WC()->payment_gateways->payment_gateways();
 
-        $fields = array();
+		$fields = array();
 
-        foreach($available_gateways as $gateway) {
-            $fields[] = array(
-                    'title'   => $gateway->method_title ? $gateway->method_title : $gateway->id,
-                    'type'    => 'multi_select_countries',
-                    'id'      => $this->id . '_' . $gateway->id,
-                );
-        }
+		foreach ( $available_gateways as $gateway ) {
+			$fields[] = array(
+				'title' => $gateway->method_title ? $gateway->method_title : $gateway->id,
+				'type'  => 'multi_select_countries',
+				'id'    => $this->id . '_' . $gateway->id,
+			);
+		}
 
-        return $fields;
-    }
-
-
-    /**
-     * Create section and include input fields in section
-     *
-     * @return array
-     */
-    public function createTabSection()
-    {
-        $section = array();
-
-        $section[] = array(
-                'title' => __( 'Country Based Payments', 'wccbp' ),
-                'desc'  => __( 'Select in which countries payment gateways will be available', 'wccbp' ),
-                'type'  => 'title',
-                'id'    => $this->id,
-            );
-
-        $section = array_merge($section, $this->createFields());
-
-        $section[] = array( 'type' => 'sectionend', 'id' => $this->id);
-
-        return $section;
-    }
+		return $fields;
+	}
 
 
-    /**
-     * Add section to tab
-     */
-    public function addSectionToTab()
-    {
-        woocommerce_admin_fields($this->createTabSection());
-    }
+	/**
+	 * Create section and include input fields in section
+	 *
+	 * @return array
+	 */
+	public function create_tab_section() {
+		$section = array();
 
-    /**
-     *  Update setting fields
-     */
-    public function updateOptions()
-    {
-        woocommerce_update_options($this->createFields());
-    }
+		$section[] = array(
+			'title' => __( 'Country Based Payments', 'wccbp' ),
+			'desc'  => __( 'Select in which countries payment gateways will be available', 'wccbp' ),
+			'type'  => 'title',
+			'id'    => $this->id,
+		);
+
+		$section = array_merge( $section, $this->create_fields() );
+
+		$section[] = array(
+			'type' => 'sectionend',
+			'id' => $this->id,
+		);
+
+		return $section;
+	}
+
+	/**
+	 * Add section to tab
+	 */
+	public function add_section_to_tab() {
+		woocommerce_admin_fields( $this->create_tab_section() );
+	}
+
+	/**
+	 *  Update setting fields
+	 */
+	public function update_options() {
+		woocommerce_update_options( $this->create_fields() );
+	}
 }
--- woocommerce-country-based-payments.php
+++ woocommerce-country-based-payments.php
@@ -12,71 +12,81 @@
  * Text Domain: wccbp
  * WC requires at least: 3.4.0
  * WC tested up to: 3.5.4
-*/
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-
-
+/**
+ * Initialize the plugin, settings page and handle plugin logic
+ */
 class WoocommerceCountryBasedPayment {
 
-    private $selected_country;
+	/**
+	 * Country ID
+	 *
+	 * @var String
+	 */
+	private $selected_country;
 
-    private $id;
+	/**
+	 * Plugin ID
+	 *
+	 * @var String
+	 */
+	private $id;
 
-    public function __construct()
-    {
-        $this->id = 'wccbp';
+	/**
+	 * Construct plugin
+	 */
+	public function __construct() {
+		$this->id = 'wccbp';
 
-        add_filter('woocommerce_get_settings_pages', array($this, 'loadSettings'), 16);
+		add_filter( 'woocommerce_get_settings_pages', array( $this, 'load_settings' ), 16 );
 
-		add_action('woocommerce_checkout_update_order_review', array($this, 'setSelectedCountry'), 10);
-		
+		add_action( 'woocommerce_checkout_update_order_review', array( $this, 'set_selected_country' ), 10 );
+
 		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 
-		// check if ajax request
+		// Check if ajax request.
 		if ( ! is_admin() && ( isset( $_REQUEST['wc-ajax'] ) && 'update_order_review' == $_REQUEST['wc-ajax'] ) ) {
-			// Fix WPML WooCommerce Multilingual error
-			add_filter( 'wcml_supported_currency_payment_gateways', array( $this, 'availablePaymentGateways' ), 90, 1 );
+			// Fix WPML WooCommerce Multilingual error.
+			add_filter( 'wcml_supported_currency_payment_gateways', array( $this, 'available_payment_gateways' ), 90, 1 );
 
-			add_filter( 'woocommerce_available_payment_gateways', array( $this, 'availablePaymentGateways' ), 10, 1 );
+			add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways' ), 10, 1 );
 		}
 
-        // check if pay_for page
-        if( !is_admin() && isset( $_GET['pay_for_order'] ) &&  true == $_GET['pay_for_order'] ) {
-            add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways_after_cancelation' ), 10, 1 );
-        }
+		// Check if pay_for page.
+		if ( ! is_admin() && isset( $_GET['pay_for_order'] ) && true == $_GET['pay_for_order'] ) {
+			add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways_after_cancelation' ), 10, 1 );
+		}
 	}
-	
+
 	/**
 	 * Load textdomain
 	 */
 	public function load_plugin_textdomain() {
-		load_plugin_textdomain( 'wccbp', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+		load_plugin_textdomain( 'wccbp', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 	}
 
 
-    /**
-     * Load admin settings
-     */
-    public function loadSettings()
-    {
-        require 'includes/admin/WCCBPSettings.php';
-        return new WCCBPSettings();
-    }
+	/**
+	 * Load admin settings
+	 */
+	public function load_settings() {
+		require 'includes/admin/WCCBPSettings.php';
+		return new WCCBPSettings();
+	}
 
 
-    /**
-     * Set selected country on Ajax request in checkout process
-     * Country code is used.
-     *
-     */
-    public function setSelectedCountry()
-    {
-        $this->selected_country = sanitize_text_field($_REQUEST['country']);
-    }
+	/**
+	 * Set selected country on Ajax request in checkout process
+	 * Country code is used.
+	 */
+	public function set_selected_country() {
+		$this->selected_country = sanitize_text_field( $_REQUEST['country'] );
+	}
 
 
 	/**
@@ -84,12 +94,12 @@ class WoocommerceCountryBasedPayment {
 	 * check if certain payment gateway is enabled for country,
 	 * if no, unset it from $payment_gateways array
 	 *
+	 * @param array $payment_gateways List of available gateways in system.
 	 * @return array with updated list of available payment gateways
 	 */
-	public function availablePaymentGateways( $payment_gateways ) {
-		
+	public function available_payment_gateways( $payment_gateways ) {
 		foreach ( $payment_gateways as $key => $value ) {
-			// check if WCML array
+			// Check if WCML array.
 			$gateway_id = ( is_object( $value ) && isset( $value->id ) ) ? $value->id : $key;
 			$gateway_availability = get_option( $this->id . '_' . $gateway_id );
 
@@ -100,74 +110,73 @@ class WoocommerceCountryBasedPayment {
 		return $payment_gateways;
 	}
 
-    /**
-     * List through available payment gateways,
-     * if customer gets redirected to the pay_for page
-     * after a payment cancellation
-     *
-     * @return array with updated list of available payment gateways
-     */
-    public function available_payment_gateways_after_cancelation( $payment_gateways )
-    {
-    	$order_id = wc_get_order_id_by_order_key( $_GET['key'] );
-    	$order = new WC_Order($order_id);
-			$billing_address = $order->get_address();
-			$this->selected_country = $billing_address['country'];
+	/**
+	 * List through available payment gateways,
+	 * if customer gets redirected to the pay_for page
+	 * after a payment cancellation
+	 *
+	 * @param array $payment_gateways List of available gateways in system.
+	 * @return array with updated list of available payment gateways
+	 */
+	public function available_payment_gateways_after_cancelation( $payment_gateways ) {
+		$order_id = wc_get_order_id_by_order_key( $_GET['key'] );
+		$order = new WC_Order( $order_id );
+		$billing_address = $order->get_address();
+		$this->selected_country = $billing_address['country'];
 
-			foreach ( $payment_gateways as $gateway ) {
-				if ( get_option( $this->id . '_' . $gateway->id ) && !in_array( $this->selected_country, get_option( $this->id . '_' . $gateway->id ) ) ) {
-					unset( $payment_gateways[$gateway->id] );
-				}
+		foreach ( $payment_gateways as $gateway ) {
+			if ( get_option( $this->id . '_' . $gateway->id ) && ! in_array( $this->selected_country, get_option( $this->id . '_' . $gateway->id ) ) ) {
+				unset( $payment_gateways[ $gateway->id ] );
 			}
+		}
 
-			return $payment_gateways;
-    }
+		return $payment_gateways;
+	}
 }
 
 /**
  * Check if WooCommerce is active
- **/
+ */
 if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
 	require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
 }
 
 if ( is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
-	// Plugin is activated
-
+	// Plugin is activated.
 	new WoocommerceCountryBasedPayment();
 }
 
 // Load Freemius SDK
 // Create a helper function for easy SDK access.
 function wcbp_fs() {
-    global $wcbp_fs;
+	global $wcbp_fs;
 
-    if ( ! isset( $wcbp_fs ) ) {
-        // Include Freemius SDK.
-        require_once dirname(__FILE__) . '/includes/freemius/start.php';
+	if ( ! isset( $wcbp_fs ) ) {
+			// Include Freemius SDK.
+			require_once dirname(__FILE__) . '/includes/freemius/start.php';
 
-        $wcbp_fs = fs_dynamic_init( array(
-            'id'                  => '2788',
-            'slug'                => 'woocommerce-country-based-payments',
-            'type'                => 'plugin',
-            'public_key'          => 'pk_cbdb518bd47595e667e3992ea2e2f',
-            'is_premium'          => false,
-            'has_addons'          => false,
-            'has_paid_plans'      => false,
-            'menu'                => array(
-                'slug'           => 'wc-settings',
-                'override_exact' => true,
-                'account'        => false,
-                'contact'        => false,
-                'support'        => false,
-                'parent'         => array(
-                    'slug' => 'woocommerce',
-                ),
-            ),
-        ) );
-    }
+			$wcbp_fs = fs_dynamic_init( array(
+					'id'                  => '2788',
+					'slug'                => 'woocommerce-country-based-payments',
+					'type'                => 'plugin',
+					'public_key'          => 'pk_cbdb518bd47595e667e3992ea2e2f',
+					'is_premium'          => false,
+					'has_addons'          => false,
+					'has_paid_plans'      => false,
+					'menu'                => array(
+							'slug'           => 'wc-settings',
+							'override_exact' => true,
+							'account'        => false,
+							'contact'        => false,
+							'support'        => false,
+							'parent'         => array(
+									'slug' => 'woocommerce',
+							),
+					),
+			) );
+	}
 
-    return $wcbp_fs;
+	return $wcbp_fs;
 }
 
 // Init Freemius.
@@ -176,7 +185,7 @@ wcbp_fs();
 do_action( 'wcbp_fs_loaded' );
 
 function wcbp_fs_settings_url() {
-    return admin_url( 'admin.php?page=wc-settings&tab=wccbp' );
+	return admin_url( 'admin.php?page=wc-settings&tab=wccbp' );
 }
 
 wcbp_fs()->add_filter( 'connect_url', 'wcbp_fs_settings_url' );
